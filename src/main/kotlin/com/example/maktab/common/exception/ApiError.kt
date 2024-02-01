000dc14353ee38ff200ca5c3ca90c0d2ff443fe1
LegendisM <makenkap@gmail.com>
2024-02-01 12:31:26 +0330

    update

--- src/main/kotlin/com/example/maktab/common/exception/ApiError.kt
+++ src/main/kotlin/com/example/maktab/common/exception/ApiError.kt
@@ -4,23 +4,23 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.server.ResponseStatusException
 
 object ApiError {
-    class Custom(
+    data class Custom(
         override val message: String,
-        status: HttpStatus = HttpStatus.OK
+        val status: HttpStatus = HttpStatus.OK
     ) : ResponseStatusException(status, message)
 
-    class BadRequest(
+    data class BadRequest(
         override val message: String = "Bad Request",
-        status: HttpStatus = HttpStatus.BAD_REQUEST
+        val status: HttpStatus = HttpStatus.BAD_REQUEST
     ) : ResponseStatusException(status, message)
 
-    class NotFound(
+    data class NotFound(
         override val message: String = "Not Found Error",
-        status: HttpStatus = HttpStatus.NOT_FOUND
+        val status: HttpStatus = HttpStatus.NOT_FOUND
     ) : ResponseStatusException(status, message)
 
-    class Unauthorized(
+    data class Unauthorized(
         override val message: String = "Unauthorized Request",
-        status: HttpStatus = HttpStatus.FORBIDDEN
+        val status: HttpStatus = HttpStatus.FORBIDDEN
     ) : ResponseStatusException(status, message)
 }
--- src/main/kotlin/com/example/maktab/common/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/example/maktab/common/exception/GlobalExceptionHandler.kt
@@ -1,25 +1,22 @@
 package com.example.maktab.common.exception
 
 import com.example.maktab.common.dto.ApiDTO
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.WebRequest
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 import org.springframework.web.server.ResponseStatusException
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import java.util.stream.Collectors
 
 @ControllerAdvice
 class GlobalExceptionHandler {
-    @ExceptionHandler(Exception::class)
-    fun resolveDefaultException(
-        exception: Exception,
-        request: WebRequest
-    ): ApiDTO.Response.Error {
-        return ApiDTO.Response.Error(
-            message = exception.message ?: "Internal Server Error",
-            status = HttpStatus.INTERNAL_SERVER_ERROR
-        )
-    }
-
     @ExceptionHandler(ResponseStatusException::class)
     fun resolveResponseStatusException(
         exception: ResponseStatusException,
@@ -28,6 +25,53 @@ class GlobalExceptionHandler {
         return ApiDTO.Response.Error(
             message = exception.message,
             status = HttpStatus.valueOf(exception.statusCode.value())
+        )
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException::class)
+    fun resolveMethodArgumentTypeMismatchException(
+        exception: MethodArgumentTypeMismatchException,
+        request: WebRequest
+    ): ApiDTO.Response.Error {
+        return ApiDTO.Response.Error(
+            message = exception.message ?: "Argument Type Mismatch",
+            status = HttpStatus.BAD_REQUEST
+        )
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun resolveMethodArgumentNotValidException(
+        exception: MethodArgumentNotValidException,
+        request: WebRequest
+    ): ApiDTO.Response.Error {
+        return ApiDTO.Response.Error(
+            message = "Validation Error",
+            status = HttpStatus.BAD_REQUEST,
+            errors = exception.fieldErrors.map {
+                "${it.field} ${it.defaultMessage ?: "has validation error"}"
+            }
+        )
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun resolveHttpMessageNotReadableException(
+        exception: HttpMessageNotReadableException,
+        request: WebRequest
+    ): ApiDTO.Response.Error {
+        return ApiDTO.Response.Error(
+            message = exception.message ?: "Validation Error (Invalid Json Property Key/Value)",
+            status = HttpStatus.BAD_REQUEST
+        )
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun resolveDefaultException(
+        exception: Exception,
+        request: WebRequest
+    ): ApiDTO.Response.Error {
+        return ApiDTO.Response.Error(
+            message = exception.message ?: "Internal Server Error",
+            status = HttpStatus.INTERNAL_SERVER_ERROR
         )
     }
 }
