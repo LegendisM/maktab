e223508f76063d3d80b7af3fdc8b04b116a3d81e
Hamidreza Mohammadi <makenkap@gmail.com>
2024-02-16 16:21:50 +0330

    S3

--- src/main/kotlin/com/example/maktab/module/storage/enums/Bucket.kt
+++ src/main/kotlin/com/example/maktab/module/storage/enums/Bucket.kt
@@ -1,8 +1,5 @@
 package com.example.maktab.module.storage.enums
 
 enum class Bucket(value: String) {
-    PRIVATE_DOCUMENTS("private_documents"),
-    PRIVATE_IMAGES("private_images"),
-    PUBLIC_IMAGES("public_images"),
-    PUBLIC_DOCUMENTS("public_documents")
+    MAKTAB_PUBLIC("maktab-public")
 }
--- src/main/kotlin/com/example/maktab/module/storage/service/S3Service.kt
+++ src/main/kotlin/com/example/maktab/module/storage/service/S3Service.kt
@@ -1,18 +1,58 @@
 package com.example.maktab.module.storage.service
 
+import com.example.maktab.module.storage.enums.Bucket
+import jakarta.annotation.PostConstruct
+import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Service
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest
+import java.io.InputStream
 
 @Service
 class S3Service {
+    private var s3Client: S3Client = S3Client.builder().region(Region.of("default")).build()
+
     init {
-        // TODO: validate existance of buckets
+        runBlocking {
+            print("Legend")
+            val buckets = getAllBuckets()
+
+            for (bucket in Bucket.entries.takeWhile { bucket -> buckets.find { bucket.name == it.name } != null }) {
+                createBucket(bucket)
+            }
+
+            for (bucket in getAllBuckets()) {
+                println(bucket.name ?: "NULL")
+            }
+        }
     }
 
-    fun uploadFile(){
+    suspend fun createBucket(bucket: Bucket): Boolean {
+        return try {
+            val result = s3Client.createBucket(
+                CreateBucketRequest
+                    .builder()
+                    .bucket(bucket.name)
+                    .build()
+            )
+            true
+        } catch (error: Exception) {
+            false
+        }
+    }
+
+    suspend fun getAllBuckets(): List<Bucket> {
+        return s3Client.listBuckets().buckets().map {
+            Bucket.valueOf(it.name())
+        }
+    }
+
+    fun uploadFile(bucket: Bucket, file: InputStream) {
 
     }
 
-    fun removeFile(){
+    fun removeFile(bucket: Bucket, key: String) {
 
     }
 }
